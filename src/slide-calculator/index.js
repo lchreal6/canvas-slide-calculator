b0733b2921595079d8215c7bbbb1a80165baa60f
lchreal6 <lchreal6@gmail.com>
2019-03-31 12:04:25 +0800

    add elastic effect

--- src/slide-calculator/index.js
+++ src/slide-calculator/index.js
@@ -20,6 +20,11 @@ class Calculator {
     element.height = this.height; 
     this.bottomLineHeight = this.height * 0.81;
     this.offsetX = 0;
+
+    this.press = false;
+    this.columnLen = 3;
+    this.columnWidth = Math.ceil(this.width / 5);
+    this.columnItemWidth = this.columnWidth / 10;
   }
 
   drawScale() {
@@ -53,29 +58,28 @@ class Calculator {
     this.ctx.closePath();
     this.ctx.stroke();
 
-    const columnWidth = this.width / 5;
-    const columnItemWidth = columnWidth / 10;
     const scaleHeight = 10;
     const scaleItemHeight = 5;
-    const Maxcolumn = columnWidth * 10;
+    const Maxcolumn = this.columnWidth * this.columnLen;
     
     this.ctx.save();
     const startX = this.offsetX;
-    console.log('startX', startX)
     this.ctx.translate(midWidth, this.bottomLineHeight)
-    for(let k = startX ; k < Maxcolumn + startX; k = k + columnWidth) {
+    for(let k = startX ; k <= Maxcolumn + startX; k = k + this.columnWidth) {
       this.ctx.beginPath();
       this.ctx.moveTo(k, 0);
       this.ctx.lineTo(k, 0 - scaleHeight);
       this.ctx.closePath();
       this.ctx.stroke();
 
-      for(let j = k; j < k + columnWidth; j = j + columnItemWidth) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(j, 0);
-        this.ctx.lineTo(j, 0 - scaleItemHeight);
-        this.ctx.closePath();
-        this.ctx.stroke();
+      for(let j = k; j < k + this.columnWidth; j = j + this.columnItemWidth) {
+        if(-startX + j <= Maxcolumn) {
+          this.ctx.beginPath();
+          this.ctx.moveTo(j, 0);
+          this.ctx.lineTo(j, 0 - scaleItemHeight);
+          this.ctx.closePath();
+          this.ctx.stroke();
+        }
       }
     }
   }
@@ -84,23 +88,56 @@ class Calculator {
     let startX = 0;
     const self = this;
     this.element.addEventListener('touchstart', function(e) {
+      self.press = true;
       console.log('start')
       const touch = e.targetTouches[0];
       startX = touch.pageX;
       e.preventDefault();
     })
     this.element.addEventListener('touchmove', function(e) {
+      self.press = true;
       const touch = e.targetTouches[0];
       let offsetX = touch.pageX - startX;
       self.offsetX = self.offsetX + offsetX * self.devicePixelRatio;
       startX = touch.pageX;
-      console.log('offsetX', self.offsetX);
+      // console.log('offsetX', self.offsetX);
       console.log('move')
       e.preventDefault();
     })
     this.element.addEventListener('touchend', function(e) {
+      self.press = false;
       e.preventDefault();
     })
+  }
+
+  adjustPosition() {
+    const selectIndex = Math.round(-this.offsetX / this.columnWidth);
+    const targetDistance = selectIndex * this.columnWidth;
+    // let adjustDistance = Math.abs(-this.offsetX -  targetDistance);
+    let adjustDistance = null;
+    // let speed = adjustDistance / 15;
+
+    if(selectIndex < 0) {
+      adjustDistance = Math.abs(-this.offsetX - 0);
+      let speed = adjustDistance / 15;
+      this.offsetX = this.offsetX - speed;
+    } else if (selectIndex > this.columnLen) {
+      adjustDistance = Math.abs(-this.offsetX - this.columnWidth * this.columnLen);
+      let speed = adjustDistance / 15;
+      this.offsetX = this.offsetX + speed;
+    } else{
+      adjustDistance = Math.abs(-this.offsetX -  targetDistance);
+      let speed = adjustDistance / 15;
+      targetDistance > -this.offsetX ? (this.offsetX = this.offsetX - speed) : (this.offsetX = this.offsetX + speed); 
+    }
+
+    if(adjustDistance < 1) {
+      this.offsetX = -selectIndex * this.columnWidth;
+      this.press = true;
+    }
+    // console.log('aaa', this.offsetX)
+    // console.log('selectIndex', selectIndex)   
+    // console.log('adjustDistance', adjustDistance);
   }
 
   render() {
@@ -108,6 +145,7 @@ class Calculator {
       this.ctx.clearRect(0,0, this.width, this.height);
       this.drawScale(); 
       this.ctx.restore();
+      !this.press && this.adjustPosition();
     }, 1000 / 60);
     this.bindEvent();
   }
