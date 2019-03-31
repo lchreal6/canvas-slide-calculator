1cbde68b6d5ce2a66fec2efa33b48b3062068f39
lchreal6 <lchreal6@gmail.com>
2019-03-31 17:14:59 +0800

    finish canvas-slide component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import Calculator from './slide-calculator/index';
 
 class App extends Component {
 
+  state = {
+    activeIndex: 0
+  }
+
   componentDidMount() {
+    const self = this;
     const options = {
-      width: 350,
+      width: window.innerWidth,
       height: 100,
-      element: '#calculator'
+      element: '#calculator',
+      data: [0, 10, 25, 33, 40, 55, 63, 78, 89, 99 ,109],
+      onSelect(data) {
+        self.setState({
+          activeIndex: data
+        })
+      }
     }
 
     const cal = new Calculator(options);
@@ -18,7 +29,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <canvas id="calculator"></canvas> 
+        <div className="index-tips"><span>activeIndex: {this.state.activeIndex}</span></div>
+        <canvas id="calculator"></canvas> 
       </div>
     );
   }
--- src/slide-calculator/index.js
+++ src/slide-calculator/index.js
@@ -1,15 +1,12 @@
-import { maxHeaderSize } from "http";
-
 class Calculator {
-
   constructor(options) {
-    console.log(options)
     const element = document.querySelector(options.element);
     this.element = element;
     const width = options.width;
     const height = options.height;
     this.ctx = element.getContext('2d');
-    console.log(this.ctx)
+    this.onSelect = options.onSelect;
+    this.data = options.data;
     this.devicePixelRatio = window.devicePixelRatio;
     element.style.width = `${width}px`;
     element.style.height = `${height}px`;
@@ -22,33 +19,33 @@ class Calculator {
     this.offsetX = 0;
 
     this.press = false;
-    this.columnLen = 3;
+    this.vx = 0;
+    this.scrollDistance = 0;
+    this.columnLen = options.data.length - 1;
     this.columnWidth = Math.ceil(this.width / 5);
     this.columnItemWidth = this.columnWidth / 10;
+    this.ctx.lineWidth = 1 * this.devicePixelRatio;
   }
 
-  drawScale() {
-    this.ctx.lineWidth = 1 * this.devicePixelRatio;
+  drawTargetArrow() {
+    const midWidth = this.width / 2;
+    const arrowHeight = this.height * 0.3;
 
+    this.ctx.strokeStyle = 'rgb(222, 222, 222)'
     this.ctx.beginPath();
     this.ctx.moveTo(0, this.bottomLineHeight);
     this.ctx.lineTo(this.width, this.bottomLineHeight);
     this.ctx.closePath();
     this.ctx.stroke();
-    
-    this.drawArrow();
-  } 
 
-  drawArrow() {
-    const midWidth = this.width / 2;
-    const arrowHeight = this.height * 0.3;
+    this.ctx.strokeStyle = 'rgb(73, 128, 219)'
     this.ctx.beginPath();
     this.ctx.moveTo(midWidth, this.bottomLineHeight); 
     this.ctx.lineTo(midWidth, arrowHeight);
     this.ctx.closePath();
     this.ctx.stroke();
 
-
+    this.ctx.fillStyle = 'rgb(73, 128, 219)'
     const arrowLineWidth = 10;
     const arrowLineHeight = arrowLineWidth * 0.70711;
     this.ctx.beginPath();
@@ -56,24 +53,48 @@ class Calculator {
     this.ctx.lineTo(midWidth + arrowLineHeight/ 2, arrowHeight);
     this.ctx.lineTo(midWidth, arrowHeight - arrowLineHeight);
     this.ctx.closePath();
-    this.ctx.stroke();
+    this.ctx.fill();
+  }
 
-    const scaleHeight = 10;
-    const scaleItemHeight = 5;
-    const Maxcolumn = this.columnWidth * this.columnLen;
+  draw() {
+    // 刻画指向标尺
+    this.drawTargetArrow();
+
+    // 刻画刻度
+    this.drawScale();
+  } 
+
+  drawScale() {
+    const midWidth = this.width / 2; 
+    const scaleHeight = this.width * 0.0267;
+    const scaleItemHeight = this.width * 0.0133;
+    const maxcolumn = this.columnWidth * this.columnLen;
     
     this.ctx.save();
     const startX = this.offsetX;
     this.ctx.translate(midWidth, this.bottomLineHeight)
-    for(let k = startX ; k <= Maxcolumn + startX; k = k + this.columnWidth) {
+    this.ctx.strokeStyle = 'rgb(222, 222, 222)';
+    this.ctx.globalCompositeOperation = 'destination-over';
+    for(let k = startX ; k <= maxcolumn + startX; k = k + this.columnWidth) {
       this.ctx.beginPath();
       this.ctx.moveTo(k, 0);
       this.ctx.lineTo(k, 0 - scaleHeight);
       this.ctx.closePath();
       this.ctx.stroke();
+      this.ctx.textAlign = 'center';
+      this.ctx.font = `${this.width * 0.033}px sans-serif`;
+      this.ctx.fillStyle = 'rgb(222, 222, 222)';
+      const index =Math.ceil((-startX + k)/this.columnWidth);
+      const data = this.data[index];
+      this.ctx.fillText(data, k, -scaleHeight - this.width * 0.0133);
+
+      if(k === 0) {
+        const selectIndex = (-startX + k)/this.columnWidth 
+        this.onSelect && this.onSelect(this.data[selectIndex])
+      }
 
       for(let j = k; j < k + this.columnWidth; j = j + this.columnItemWidth) {
-        if(-startX + j <= Maxcolumn) {
+        if(-startX + j <= maxcolumn) {
           this.ctx.beginPath();
           this.ctx.moveTo(j, 0);
           this.ctx.lineTo(j, 0 - scaleItemHeight);
@@ -84,12 +105,17 @@ class Calculator {
     }
   }
 
+  // 绑定触摸事件
   bindEvent() {
     let startX = 0;
     const self = this;
+    let startTime = null;
+    let endTime = null;
     this.element.addEventListener('touchstart', function(e) {
       self.press = true;
-      console.log('start')
+      startTime = new Date().getTime();
+      self.scrollDistance = 0;
+      self.vx = 0;
       const touch = e.targetTouches[0];
       startX = touch.pageX;
       e.preventDefault();
@@ -99,35 +125,63 @@ class Calculator {
       const touch = e.targetTouches[0];
       let offsetX = touch.pageX - startX;
       self.offsetX = self.offsetX + offsetX * self.devicePixelRatio;
+      
+      if(!self.scrollFlag) {
+        if(offsetX > 0 && self.offsetX > self.width * 0.1) {
+          self.offsetX  = self.width * 0.1;
+        } else if (offsetX < 0 && self.offsetX < -self.columnWidth * self.columnLen - self.width * 0.1) {
+          self.offsetX  = -self.columnWidth * self.columnLen - self.width * 0.1;
+        }
+      }
+       
+
       startX = touch.pageX;
-      // console.log('offsetX', self.offsetX);
-      console.log('move')
+      self.vx = offsetX;
       e.preventDefault();
     })
     this.element.addEventListener('touchend', function(e) {
       self.press = false;
+      endTime = new Date().getTime();
+      const time = endTime - startTime;
+      
+      if(time < 300) {
+        if(self.vx < - 0) {
+          self.scrollDistance = - self.columnWidth * self.columnLen * 20 * self.devicePixelRatio / time;
+          if(self.scrollDistance + self.offsetX <  -self.columnWidth * self.columnLen - self.width * 0.1) {
+            self.scrollDistance = -self.columnWidth * self.columnLen - self.width * 0.1 - self.offsetX;
+          }
+        } else if (self.vx > 0) {
+          self.scrollDistance = self.columnWidth * self.columnLen * 20 * self.devicePixelRatio / time;
+          if(self.scrollDistance + self.offsetX > self.width * 0.1) {
+            self.scrollDistance = self.width * 0.1 - self.offsetX;
+          }
+        }
+        self.scrollFlag = true;
+      } else {
+        self.scrollFlag = false;
+      }
+
       e.preventDefault();
     })
   }
 
+  // 滑动停止后，调整回到合适的位置
   adjustPosition() {
     const selectIndex = Math.round(-this.offsetX / this.columnWidth);
     const targetDistance = selectIndex * this.columnWidth;
-    // let adjustDistance = Math.abs(-this.offsetX -  targetDistance);
     let adjustDistance = null;
-    // let speed = adjustDistance / 15;
 
     if(selectIndex < 0) {
       adjustDistance = Math.abs(-this.offsetX - 0);
-      let speed = adjustDistance / 15;
+      let speed = adjustDistance / 5;
       this.offsetX = this.offsetX - speed;
     } else if (selectIndex > this.columnLen) {
       adjustDistance = Math.abs(-this.offsetX - this.columnWidth * this.columnLen);
-      let speed = adjustDistance / 15;
+      let speed = adjustDistance / 5;
       this.offsetX = this.offsetX + speed;
     } else{
       adjustDistance = Math.abs(-this.offsetX -  targetDistance);
-      let speed = adjustDistance / 15;
+      let speed = adjustDistance / 5;
       targetDistance > -this.offsetX ? (this.offsetX = this.offsetX - speed) : (this.offsetX = this.offsetX + speed); 
     }
 
@@ -135,17 +189,25 @@ class Calculator {
       this.offsetX = -selectIndex * this.columnWidth;
       this.press = true;
     }
-    // console.log('aaa', this.offsetX)
-    // console.log('selectIndex', selectIndex)   
-    // console.log('adjustDistance', adjustDistance);
+  }
+
+  scrollMove() {
+    let speed = this.scrollDistance / 10;
+    this.scrollDistance = this.scrollDistance - speed;
+    this.offsetX = this.offsetX + speed;
+
+    if(Math.abs(this.scrollDistance) < 1) {
+      this.scrollFlag = false;
+    }
   }
 
   render() {
     setInterval(() => {
       this.ctx.clearRect(0,0, this.width, this.height);
-      this.drawScale(); 
+      this.draw(); 
       this.ctx.restore();
-      !this.press && this.adjustPosition();
+      this.scrollFlag && this.scrollMove();
+      !this.press && !this.scrollFlag && this.adjustPosition();
     }, 1000 / 60);
     this.bindEvent();
   }
